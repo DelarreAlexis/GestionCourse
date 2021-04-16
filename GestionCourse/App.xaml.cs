9126d1643f77e8231a8784d2ac73fccea286283f
Alexis Delarre <adelarre@amo-technologies.com>
2021-04-16 19:38:14 +0200

    Gestion Course sans MVVM

---
+++ GestionCourse/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GestionCourse
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new ListeCourse();
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
